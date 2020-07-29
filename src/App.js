87804df5ac622fbd4cb8cd9e13ceea126cb64686
Hsueh Yen Neo <neo.hsuehyen@gmail.com>
2020-07-29 15:01:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="Header">Epic Meme Generator</div>
+        <div className="Main">
+        </div>    
+    </div>
+  );
+}
+
+export default App;
