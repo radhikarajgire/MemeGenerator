2e8e63c4b67cd9efdc665f4019035f18bd522477
Erick Jansen <janseniter@gmail.com>
2020-08-10 16:32:19 +0200

    change meme

--- src/Components/Meme/Meme.js
+++ src/Components/Meme/Meme.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import './Meme.css';
 
 import { StateContext } from '../../context';
--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, {createContext, useState, useEffect} from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const StateContext = createContext({});
 
@@ -7,19 +7,25 @@ const ContextProvider = ({ children }) => {
   const [selectedMemeSrc, setSelectedMemeSrc] = useState(null);
 
   useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
+    fetch('https://api.imgflip.com/get_memes')
       .then((response) => response.json())
       .then((json) => {
         const allMemes = json.data.memes.slice(0, 90);
         setMemes(allMemes);
       })
       .catch((error) => console.log(error.message));
-  }
-    
-  );
-  const [imgData, setImgData] = useState(null); 
+  }, []);
+  const [imgData, setImgData] = useState(null);
   return (
-    <StateContext.Provider value={{ memes, selectedMemeSrc, setSelectedMemeSrc, imgData, setImgData }}>
+    <StateContext.Provider
+      value={{
+        memes,
+        selectedMemeSrc,
+        setSelectedMemeSrc,
+        imgData,
+        setImgData,
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
